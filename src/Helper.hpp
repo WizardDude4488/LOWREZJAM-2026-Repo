0c07db999a531552f9abd02b869e8f505f0542d4
tabbul <turtleproductions4488@gmail.com>
2026-08-01 23:40:01 -0400

    Added helper functions, function to create animation rectangles

---
+++ src/Helper.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <vector>
+
+#include "raylib.h"
+
+namespace Helper {
+	// Go left to right, top to bottom creating rects representing regions of the spritesheet
+	std::vector<Rectangle> create_spritesheet_frames(int frame_width, int frame_height, int image_width, int image_height);
+}
