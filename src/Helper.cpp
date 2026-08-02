069222399f73abbc84bde231ab8e8ebd83b807ae
tabbul <turtleproductions4488@gmail.com>
2026-08-01 23:40:01 -0400

    Added helper functions, function to create animation rectangles

---
+++ src/Helper.cpp
@@ -0,0 +1,19 @@
+#include <cmath>
+
+#include "Helper.hpp"
+
+std::vector<Rectangle> Helper::create_spritesheet_frames(int frame_width, int frame_height, int image_width, int image_height) {
+	std::vector<Rectangle> rectangles;
+	
+	// Exclude border space that doesn't take up a full frame
+	int max_width = std::floor(static_cast<double>(image_width) / static_cast<double>(frame_width));
+	int max_height = std::floor(static_cast<double> (image_height) / static_cast<double>(frame_height));
+							// Adjust top-left
+	for (int y = 0; y <= max_height - frame_height; y += frame_height) {
+		for (int x = 0; x <= max_width - frame_width; x += frame_width) {
+			rectangles.push_back(Rectangle{ static_cast<float>(x), static_cast<float>(y), static_cast<float>(frame_width), static_cast<float>(frame_height)});
+		}
+	}
+
+	return rectangles;
+}
